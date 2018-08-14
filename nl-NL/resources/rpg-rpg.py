fde31be651be5cc17008ddc485c6fdfbe9408a78
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2018-08-14 10:53:26 +0100

    New translations rpg-rpg.py (Dutch)

--- nl-NL/resources/rpg-rpg.py
+++ nl-NL/resources/rpg-rpg.py
@@ -2,85 +2,85 @@
 
 # Replace RPG starter project with this code when new instructions are live
 
-def showInstructions():
-  #print a main menu and the commands
+def toonInstructies():
+  #laat een hoofdmenu en de commando's zien
   print('''
-RPG Game
+RPG Spel
 ========
-Commands:
-  go [direction]
-  get [item]
+Commando‘s:
+  ga [richting]
+  pak [voorwerp]
 ''')
 
-def showStatus():
-  #print the player's current status
+def toonStatus():
+  #laat de huidige status van de speler zien
   print('---------------------------')
-  print('You are in the ' + currentRoom)
-  #print the current inventory
-  print('Inventory : ' + str(inventory))
-  #print an item if there is one
-  if "item" in rooms[currentRoom]:
-    print('You see a ' + rooms[currentRoom]['item'])
+  print('Je bent in de ' + dezeKamer)
+  #laat de huidige inventaris zien
+  print("inventaris : " + str(inventaris))
+  #laat een voorwerp zien als er een is
+  if "voorwerp" in kamers[dezeKamer]:
+    print('Je ziet een ' + kamers[dezeKamer]['voorwerp'])
   print("---------------------------")
 
-#an inventory, which is initially empty
-inventory = []
+#een lege inventaris
+inventaris = []
 
-#a dictionary linking a room to other rooms
-rooms = {
+#een woordenboek die een kamer verbindt met andere kamers
+kamers = {
 
-            'Hall' : { 
-                  'south' : 'Kitchen'
+            'Hal' : { 
+                  'zuid' : 'Keuken'
                 },
 
-            'Kitchen' : {
-                  'north' : 'Hall'
+            'Keuken' : {
+                  'noord' : 'Hal'
                 }
 
          }
 
-#start the player in the Hall
-currentRoom = 'Hall'
+#laat de speler in de hal beginnen
+dezeKamer = 'Hal'
 
-showInstructions()
+toonInstructies()
 
-#loop forever
+#Voor altijd herhalen
 while True:
 
-  showStatus()
+  toonStatus()
 
-  #get the player's next 'move'
-  #.split() breaks it up into an list array
-  #eg typing 'go east' would give the list:
-  #['go','east']
-  move = ''
-  while move == '':  
-    move = input('>')
+  #vraag de volgende stap van de speler op
+  #.split() splitst het op in een gerangschikte lijst
+  #zo zal 'ga oost' deze lijst geven:
+  #['ga','oost']
+  beweeg = ''
+  while beweeg == '':  
+    beweeg = input('>')
     
-  move = move.lower().split()
+  beweeg = beweeg.lower().split()
 
-  #if they type 'go' first
-  if move[0] == 'go':
-    #check that they are allowed wherever they want to go
-    if move[1] in rooms[currentRoom]:
-      #set the current room to the new room
-      currentRoom = rooms[currentRoom][move[1]]
-    #there is no door (link) to the new room
+  #als eerst 'ga' wordt getypt
+  if beweeg[0] == 'ga':
+    #kijk dan of alle richtingen beschikbaar zijn
+    if beweeg[1] in kamers[dezeKamer]:
+      #de huidige kamer wordt de nieuwe kamer
+      dezeKamer = kamers[dezeKamer][beweeg[1]]
+    #er is geen deur (verbinding) naar de nieuwe kamer
     else:
-        print('You can\'t go that way!')
+        print('Je kunt zo niet gaan!')
 
-  #if they type 'get' first
-  if move[0] == 'get' :
-    #if the room contains an item, and the item is the one they want to get
-    if "item" in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
-      #add the item to their inventory
-      inventory += [move[1]]
-      #display a helpful message
-      print(move[1] + ' got!')
-      #delete the item from the room
-      del rooms[currentRoom]['item']
-    #otherwise, if the item isn't there to get
+  #als eerst 'pak' wordt getypt
+  if beweeg[0] == 'pak' :
+    #als de kamer een voorwerp bevat en het je wilt het hebben
+    if 'voorwerp' in kamers[dezeKamer] and beweeg[1] in kamers[dezeKamer]['voorwerp']:
+      #voeg het voorwerp toe aan de inventaris
+      inventaris += [beweeg[1]]
+      #laat een verklarend berichtje zien
+      print(beweeg[1] + ' hebbes!')
+      #haal het voorwerp uit de kamer
+      del kamers[dezeKamer]['voorwerp']
+    #of, als er geen voorwerp is
     else:
-      #tell them they can't get it
-      print('Can\'t get ' + move[1] + '!')
+      #geef aan dat er niks te pakken is
+      print('Kan' + beweeg[1] + 'niet pakken!')
 
