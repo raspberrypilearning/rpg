36f035a1a3cfdc086e15126b33d2a72115feb370
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2019-04-30 10:03:08 +0100

    New translations rpg-rpg.py (Korean)

--- ko-KR/resources/rpg-rpg.py
+++ ko-KR/resources/rpg-rpg.py
@@ -49,9 +49,9 @@ while True:
 
   showStatus()
 
-  #get the player's next 'move'
-  #.split() breaks it up into an list array
-  #eg typing 'go east' would give the list:
+  #플레이어를 'move'로 이동함
+  #.split() 함수는 문자열을 구분함
+  #e.g. 'go east'는 아래와 같이 리스트에 삽입됨:
   #['go','east']
   move = ''
   while move == '':  
@@ -59,28 +59,28 @@ while True:
     
   move = move.lower().split()
 
-  #if they type 'go' first
+  #만약에 'go'가 입력되면
   if move[0] == 'go':
-    #check that they are allowed wherever they want to go
+    #go 다음에 입력된 문자열이 올바른 방인지를 체크
     if move[1] in rooms[currentRoom]:
-      #set the current room to the new room
+      #현재 방을 새로운 방으로 옮김
       currentRoom = rooms[currentRoom][move[1]]
-    #there is no door (link) to the new room
+    #만약 방을 해당 방향으로 옮길 수 없다면,
     else:
-        print('You can\'t go that way!')
+        print('이곳으로 갈 수 없습니다!')
 
-  #if they type 'get' first
+  #만약에 'get'이 입력되면
   if move[0] == 'get' :
-    #if the room contains an item, and the item is the one they want to get
+    #만약 방 안에 아이템이 있고, 플레이어가 입력한 아이템이 존재한다면
     if "item" in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
-      #add the item to their inventory
+      #아이템을 인벤토리에 추가함
       inventory += [move[1]]
-      #display a helpful message
-      print(move[1] + ' got!')
-      #delete the item from the room
+      #플레이어에게 메시지 출력
+      print(move[1] + '를 받았습니다!')
+      #아이템을 방에서 제거
       del rooms[currentRoom]['item']
-    #otherwise, if the item isn't there to get
+    #만약에 아이템이 없다면
     else:
-      #tell them they can't get it
-      print('Can\'t get ' + move[1] + '!')
+      #아이템이 없다라고 메시지 출력
+      print(move[1] + '아이템을 획득할 수 없습니다!')
 
