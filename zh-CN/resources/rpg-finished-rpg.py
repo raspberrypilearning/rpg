e56e75479c7a58cb0346cd71e01a22f3c6cf119b
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2019-05-18 01:42:05 +0100

    New translations rpg-finished-rpg.py (Chinese Simplified)

--- zh-CN/resources/rpg-finished-rpg.py
+++ zh-CN/resources/rpg-finished-rpg.py
@@ -36,72 +36,72 @@ rooms = {
                   'item'  : '钥匙'
                 },        
 
-            'Kitchen' : { 'north' : 'Hall',
-                  'item'  : 'monster'
+            '厨房' : { 'north' : '大厅',
+                  'item'  : '怪物'
                 },
                 
-            'Dining Room' : { 'west'  : 'Hall',
-                  'south' : 'Garden',
-                  'item'  : 'potion'
+            '餐厅' : { 'west'  : '大厅',
+                  'south' : '花园',
+                  'item'  : '魔法药水'
               
                 },
                 
-            'Garden' : { 'north' : 'Dining Room' }
+            '花园' : { 'north' : '餐厅' }
 
          }
 
-#start the player in the Hall
-currentRoom = 'Hall'
+# 玩家从大厅开始游戏
+currentRoom = '大厅'
 
 showInstructions()
 
-#loop forever
+# 永久循环
 while True:
 
   showStatus()
 
-  #get the player's next 'move'
-  #.split() breaks it up into an list array
-  #eg typing 'go east' would give the list:
-  #['go','east']
+  # 取得玩家的下一个行动
+  # .split()方法将字符串在空格处分割，并将结果以阵列输出
+  # 例如：输入"go east“将产生如下阵列
+  # ['go','east']
   move = ''
   while move == '':  
     move = input('>')
     
   move = move.lower().split()
 
-  #if they type 'go' first
+  # 如果玩家输入'go'指令
   if move[0] == 'go':
-    #check that they are allowed wherever they want to go
+    # 检查玩家是否被允许走入他们所输入的方向
     if move[1] in rooms[currentRoom]:
-      #set the current room to the new room
+      # 将当前房间换成新进入的房间
       currentRoom = rooms[currentRoom][move[1]]
-    #there is no door (link) to the new room
+    # 那里没有通向另一房间的门（连接）
     else:
-      print('You can\'t go that way!')
+      print('你不能往那里走！')
 
-  #if they type 'get' first
+  # 如果玩家输入'get'指令
   if move[0] == 'get' :
-    #if the room contains an item, and the item is the one they want to get
+    # 如果房间里有一件物品，那这件物品是玩家想要拿到的
     if 'item' in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
-      #add the item to their inventory
+      # 将物品加入玩家的物品清单
       inventory += [move[1]]
-      #display a helpful message
-      print(move[1] + ' got!')
-      #delete the item from the room
+      # 显示一条有用的消息
+      print('拿到了 ' + move[1] + '！')
+      # 将物品从房间里去除
       del rooms[currentRoom]['item']
-    #otherwise, if the item isn't there to get
+    # 否则，如果没有可拿的物品
     else:
-      #tell them they can't get it
-      print('Can\'t get ' + move[1] + '!')
+      # 告诉玩家不能拿到物品
+      print('无法拿到 ' + move[1] + '！')
 
-  # player loses if they enter a room with a monster
-  if 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item']:
-    print('A monster has got you... GAME OVER!')
+  # 玩家如果进入一个有怪物的房间，则游戏失败
+  if 'item' in rooms[currentRoom] and '怪物' in rooms[currentRoom]['item']:
+    print('一个怪物抓住你了... 游戏结束！')
     break
 
-  # player wins if they get to the garden with a key and a potion
-  if currentRoom == 'Garden' and 'key' in inventory and 'potion' in inventory:
-    print('You escaped the house... YOU WIN!')
+  # 如果玩家得到钥匙和魔法药水并到达花园，就赢了
+  if currentRoom == '花园' and '钥匙' in inventory and '魔法药水' in inventory:
+    print('你成功逃离房子... 你赢了！')
     break
   
