4dad2b50797833a94e31214400a5a482e88aaef7
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-01-17 14:16:09 +0000

    New translations rpg.py (Spanish)

--- es-ES/resources/rpg.py
+++ es-ES/resources/rpg.py
@@ -2,35 +2,35 @@
 
 # Reemplaza el proyecto inicial RPG con este codigo cuando aparezcan nuevas instrucciones
 
-def showInstructions():
+def mostrarInstrucciones():
   #imprime un menu principal y los comandos
   print('''
 Juego RPG
-========
+=========
 Comandos:
   ir [direction]
   coger [item]
 ''')
 
-def showStatus():
+def mostrarEstado():
   #Imprime el estado actual del jugador
   print('---------------------------')
-  print("Estás en el / la " + currentRoom)
+  print("Estás en el / la " + habitacionActual)
   #Imprime el inventario actual
-  print("Inventario: " + str(inventory))
+  print("Inventario: " + str(inventario))
   #imprime un objeto si hay uno
-  if "item" in rooms[currentRoom]:
-    print('Puedes ver ' + rooms[currentRoom]['item'])
+  if "objeto" in habitaciones[habitacionActual]:
+    print('Puedes ver una ' + habitaciones[habitacionActual]['objeto'])
   print("---------------------------")
 
-#un inventario, que esta vacio al principio
-inventory = []
+#un inventario, que está vacío al principio
+inventario = []
 
-#un diccionario que une una habitacion a las posiciones de las otras habitaciones
-rooms = {
+#un diccionario que une una habitación a las posiciones de las otras habitaciones
+habitaciones = {
 
             'Sala' : { 
-                  'sur' : "Cocina'
+                  'sur' : 'Cocina'
                 },
 
             'Cocina' : {
@@ -40,47 +40,47 @@ rooms = {
          }
 
 #comienza con el jugador en la Sala
-currentRoom = 'Sala'
+habitacionActual = 'Sala'
 
-showInstructions()
+mostrarInstrucciones()
 
 #Repetir indefinidamente
 while True:
 
-  showStatus()
+  mostrarEstado()
 
   #obtiene el siguiente movimiento del jugador
   #.split() lo separa en una lista
   #por ejemplo escribir 'ir este' va a dar la lista:
   #['ir','este']
-  move = ''
-  while move == '':  
-    move = input('>')
+  movimiento = ''
+  while movimiento == '':  
+    movimiento = input('>')
     
-  move = move.lower().split()
+  movimiento = movimiento.lower().split()
 
   #si escriben 'ir' primero
-  if move[0] == 'ir':
+  if movimiento[0] == 'ir':
     #verifica que esta permitido ir a donde quieren ir
-    if move[1] in rooms[currentRoom]:
+    if movimiento[1] in habitaciones[habitacionActual]:
       #haz que la habitacion en la que esta el jugador sea la nueva habitacion
-      currentRoom = rooms[currentRoom][move[1]]
+      habitacionActual = habitaciones[habitacionActual][movimiento[1]]
     #si no hay una puerta (conectando) hacia donde quieren ir
     else:
-        print('¡No puedes ir en esa direccion!')
+        print('¡No puedes ir en esa dirección!')
 
   #si escriben 'coger' primero
-  if move[0] == 'coger' :
+  if movimiento[0] == 'coger' :
     #si la habitacion contiene un objeto y ese objeto es el que el jugador quiere coger
-    if 'item' in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
+    if 'objeto' in habitaciones[habitacionActual] and movimiento[1] in habitaciones[habitacionActual]['objeto']:
       #añade el objeto al inventario
-      inventory += [move[1]]
+      inventario += [movimiento[1]]
       #muestra un mensaje de ayuda
-      print('¡Ahora tienes un/una ' + move[1] + '!')
+      print('¡Ahora tienes un/una ' + movimiento[1] + '!')
       #borra el objeto de la habitación
-      del rooms[currentRoom]['item']
+      del habitaciones[habitacionActual]['objeto']
     #si el objeto que se quiere no esta en la habitación
     else:
       #diles que no pueden cogerlo
-      print('¡No puedes coger el/la ' + move[1] + '!')
+      print('¡No puedes coger el/la ' + movimiento[1] + '!')
 
