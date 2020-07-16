8d10c1a010ec863d0eaa7197498203c444a15f10
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-07-16 12:54:28 +0100

    New translations rpg-complete.py (Marathi)

---
+++ mr-IN/solutions/rpg-complete.py
@@ -0,0 +1,122 @@
+#!/bin/python3
+
+def showInstructions():
+    #print a main menu and the commands
+    print('''
+RPG Game
+========
+
+Get to the Garden with a key and a potion
+Avoid the monsters!
+
+You are getting tired, each time you move you loose 1 health point. 
+
+Commands:
+  go [direction]
+  get [item]
+''')
+
+def showStatus():
+  #print the player's current status
+  print('---------------------------')
+  print(name + ' is in the ' + currentRoom)
+  print("Health : " + str(health))
+  #print the current inventory
+  print("Inventory : " + str(inventory))
+  #print an item if there is one
+  if "item" in rooms[currentRoom]:
+    print('You see a ' + rooms[currentRoom]['item'])
+  print("---------------------------")
+
+# setup the game
+name = None
+health = 5
+currentRoom = 'Hall'
+inventory = []
+
+#-# YOUR CODE GOES HERE #-#
+# Load data from the file
+
+#a dictionary linking a room to other room positions
+rooms = {
+
+            'Hall' : { 'south' : 'Kitchen',
+                  'east'  : 'Dining Room',
+                  'item'  : 'key'
+                },
+
+            'Kitchen' : { 'north' : 'Hall',
+                  'item'  : 'monster'
+                },
+
+            'Dining Room' : { 'west'  : 'Hall',
+                  'south' : 'Garden',
+                  'item'  : 'potion'
+
+                },
+
+            'Garden' : { 'north' : 'Dining Room' }
+
+         }
+
+# ask the player their name
+if name is None:
+  name = input("What is your name Adventurer? ")
+  showInstructions()
+
+#loop forever
+while True:
+
+  showStatus()
+
+  #get the player's next 'move'
+  #.split() breaks it up into an list array
+  #eg typing 'go east' would give the list:
+  #['go','east']
+  move = ''
+  while move == '':
+    move = input('>')
+
+  move = move.lower().split()
+
+  #if they type 'go' first
+  if move[0] == 'go':
+    health = health - 1
+    #check that they are allowed wherever they want to go
+    if move[1] in rooms[currentRoom]:
+      #set the current room to the new room
+      currentRoom = rooms[currentRoom][move[1]]
+    #there is no door (link) to the new room
+    else:
+      print('You can\'t go that way!')
+
+  #if they type 'get' first
+  if move[0] == 'get' :
+    #if the room contains an item, and the item is the one they want to get
+    if "item" in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
+      #add the item to their inventory
+      inventory += [move[1]]
+      #display a helpful message
+      print(move[1] + ' got!')
+      #delete the item from the room
+      del rooms[currentRoom]['item']
+    #otherwise, if the item isn't there to get
+    else:
+      #tell them they can't get it
+      print('Can\'t get ' + move[1] + '!')
+
+  #player loses if they enter a room with a monster
+  if 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item']:
+    print('A monster has got you... GAME OVER!')
+    break
+
+  if health == 0:
+    print('You collapse from exhaustion... GAME OVER!')
+
+  #player wins if they get to the garden with a key and a potion
+  if currentRoom == 'Garden' and 'key' in inventory and 'potion' in inventory:
+    print('You escaped the house... YOU WIN!')
+    break
+
+  #-# YOUR CODE GOES HERE #-#
+  # Save game data to the file
